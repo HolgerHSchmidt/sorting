afe4544a3d2c766ff05fae84ac883de48c83dba2
Holger Schmidt <Holger.Schmidt@multicom.de>
2020-01-15 10:09:13 +0100

    Initial Commit

---
+++ sorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sorting
+//
+//  Created by Holger Schmidt on 15.01.20.
+//  Copyright © 2020 Holger Schmidt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
